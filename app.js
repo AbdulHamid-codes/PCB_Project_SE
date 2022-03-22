77bd03d21036e65ed80fb9e8b3f3b0b9bf5ffde7
AbdulHamid-codes <abdulhamidlaspure@gmail.com>
2022-03-22 17:20:47 +0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,23 @@
+// jshint: version 6
+
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/homePage.html");
+});
+
+app.get("/admin", function(req, res){
+    res.sendFile(__dirname + "/admin/login.html");
+});
+
+app.listen(3000, function(){
+    console.log("Server is running at port 3000");
+});
+
