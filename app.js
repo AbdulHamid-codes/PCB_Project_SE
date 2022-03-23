e6655edc3f1a6154de8948f2e84d02098c6a2fb7
RohanSaeed0411 <rohan.saeed.638@gmail.com>
2022-03-23 15:51:35 +0500

    Removed sections, added link of admin to home

--- app.js
+++ app.js
@@ -6,18 +6,25 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    res.sendFile(__dirname + "/homePage.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/homePage.html");
 });
 
-app.get("/admin", function(req, res){
-    res.sendFile(__dirname + "/admin/login.html");
+app.get("/admin", function (req, res) {
+  res.sendFile(__dirname + "/admin/login.html");
 });
 
-app.listen(3000, function(){
-    console.log("Server is running at port 3000");
+app.post("/admin", function (req, res) {
+  res.sendFile(__dirname + "/admin/login.html");
 });
 
+app.post("/adminpage", function (req, res) {
+  res.sendFile(__dirname + "/admin/adminpage .html");
+});
+
+app.listen(3000, function () {
+  console.log("Server is running at port 3000");
+});
