ae6946667839ae827fde3c54709117ab88c05540
RohanSaeed0411 <rohan.saeed.638@gmail.com>
2022-03-24 23:50:31 +0500

    Category section done

--- app.js
+++ app.js
@@ -28,9 +28,35 @@ app.post("/admin", function (req, res) {
 app.post("/adminpage", function (req, res) {
   res.sendFile(__dirname + "/admin/adminpage.html");
 });
-app.post("/create-category", function (req, res) {
+app.get("/create-category", function (req, res) {
   res.sendFile(__dirname + "/admin/createCategory.html");
 });
+app.post("/create-category", function (req, res) {
+  res.send("<h2>CATEGORY HAS BEEN CREATED.</h2>");
+});
+
+app.get("/remove-category", function (req, res) {
+  res.sendFile(__dirname + "/admin/removeCategory.html");
+});
+app.post("/remove-category", function (req, res) {
+  res.send("<h2>CATEGORY HAS BEEN REMOVED.</h2>");
+});
+
+app.get("/allocate-category", function (req, res) {
+  res.sendFile(__dirname + "/admin/allocateCategory.html");
+});
+app.post("/allocate-category", function (req, res) {
+  res.send("<h2>CATEGORY HAS BEEN ALLOCATED TO THE PLAYER.</h2>");
+});
+
+app.get("/remove-player-category", function (req, res) {
+  res.sendFile(__dirname + "/admin/removePlayerCategory.html");
+});
+app.post("/remove-player-category", function (req, res) {
+  res.send("<h2>PLAYER HAS BEEN REMOVED FROM THE CATEGORY.</h2>");
+});
+
+
 
 app.listen(3000, function () {
   console.log("Server is running at port 3000");
