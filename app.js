bd968acee32562774e0c85bece8bf11cb7a6090d
AbdulHamid-codes <abdulhamidlaspure@gmail.com>
2022-03-24 22:49:12 +0500

    Connected to Database

--- app.js
+++ app.js
@@ -2,12 +2,21 @@
 
 const express = require("express");
 const request = require("request");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// Database connection
+mongoose.connect("mongodb://127.0.0.1/PCB_db", {useNewUrlParser: true});
+mongoose.connection
+    .once( 'open' ,  () => console.log('Database Connected"')) 
+    .on( 'error', (error) => {
+        console.log("Your error: "  + error);
+    });
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/homePage.html");
@@ -17,7 +26,15 @@ app.get("/admin", function (req, res) {
   res.sendFile(__dirname + "/admin/login.html");
 });
 
+app.get("/players", function(req, res){
+  res.send("Hello reached player");
+});
+
 app.get("/adminpage", function (req, res) {
+  res.sendFile(__dirname + "/admin/adminpage.html");
+});
+
+app.post("/adminpage", function (req, res) {
   res.sendFile(__dirname + "/admin/adminpage.html");
 });
 
@@ -25,12 +42,6 @@ app.post("/admin", function (req, res) {
   res.sendFile(__dirname + "/admin/login.html");
 });
 
-app.post("/adminpage", function (req, res) {
-  res.sendFile(__dirname + "/admin/adminpage.html");
-});
-app.post("/create-category", function (req, res) {
-  res.sendFile(__dirname + "/admin/createCategory.html");
-});
 
 app.listen(3000, function () {
   console.log("Server is running at port 3000");
